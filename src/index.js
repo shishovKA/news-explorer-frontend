db3afe96f5c6b4fa4b91e1aa16f332ba2b241a6c
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-12-12 10:07:10 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 
-
 //РАЗДЕЛ: import
-
 import "./style.css";
 
-console.log('main page diplom')
+const popup = document.querySelector('.popup')
+const closeBtn = document.querySelector('.popup__close');
+const authBtn = document.querySelector('.header__button_auth');
 
+authBtn.addEventListener('click', () => {
+    console.log('open');
+    popup.classList.toggle('popup_is-opened');
+})
+
+if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        console.log('close');
+        popup.classList.toggle('popup_is-opened');
+    })
+}
 
