d80ce8a571dfc640d43a87774ebbef1ecff15e47
Konstantin Shishov <shishov.k.a@yandex.ru>
2021-02-01 02:20:08 +0300

    update certs

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,3 @@
 //export const newsApiKey = '36d3172141db46a0bb3779bc2b411589';
-export const backUrl = 'https://84.201.179.172';
+export const backUrl = 'https://shishovnews.students.nomoreparties.xyz';
 export const newsApiKey = '9b1b9c8020ec46e8867a9b59a4a11288';
